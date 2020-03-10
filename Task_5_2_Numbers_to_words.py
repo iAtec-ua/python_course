49c0043b07ce3d98c1f6ca51663a65698436e0ad
iAtec <atec.khm@gmail.com>
2020-03-10 13:06:53 +0200

    numbers to words

--- Task_5_2_Numbers_to_words.py
+++ Task_5_2_Numbers_to_words.py
@@ -54,50 +54,57 @@ numbers_for_thousands = {
     "4": " чотири"
 }
 
-# Prompt user to enter a number
-prompt_number = input("Введіть число (від 1 до 999999): ")
-number_length = len(prompt_number)
-number_list = list(prompt_number)
 
-# Check for Errors
-while True:
-    try:
-        int(prompt_number) or number_length > 4
-        break
-    except:
-        # Not a valid number
-        prompt_number = input("Будь ласка, введіть ціле число від 1 до 999999: ")
-
-int_prompt_number = int(prompt_number)
-
-if int_prompt_number < 20:
-    print(f"Ваша цифра:{ordinary_numbers[prompt_number]}!")
-elif int_prompt_number < 100:
-    print(f"Ваша цифра:{decimal_numbers[number_list[0]]}{ordinary_numbers[number_list[1]]}!")
-elif int_prompt_number < 1000:
-    if int(number_list[-2]) == 1:
-        decimal_list = number_list[-2:]
-        decimals = str("".join(decimal_list))
-        print(f"Ваша цифра:{hundreds[number_list[0]]}{ordinary_numbers[decimals]}!")
+def user_prompt(msg):
+    prompt_number = input(msg)
+    if not prompt_number.isdigit():
+        return user_prompt("Вы ввели не число. Введіть число: ")
+    if int(prompt_number) in range(1, 9999):
+        number_length = len(prompt_number)
+        number_list = list(prompt_number)
+        int_prompt_number = int(prompt_number)
+        if int_prompt_number < 20:
+            print(f"Ваша цифра:{ordinary_numbers[prompt_number]}!")
+        elif int_prompt_number < 100:
+            print(f"Ваша цифра:{decimal_numbers[number_list[0]]}{ordinary_numbers[number_list[1]]}!")
+        elif int_prompt_number < 1000:
+            if int(number_list[-2]) == 1:
+                decimal_list = number_list[-2:]
+                decimals = str("".join(decimal_list))
+                print(f"Ваша цифра:{hundreds[number_list[0]]}{ordinary_numbers[decimals]}!")
+            else:
+                print(
+                    f"Ваша цифра:{hundreds[number_list[0]]}{decimal_numbers[number_list[1]]}{ordinary_numbers[number_list[2]]}!")
+        elif int_prompt_number < 10000:
+            if int(number_list[0]) == 1:
+                if int(number_list[-2]) == 1:
+                    decimal_list = number_list[-2:]
+                    decimals = str("".join(decimal_list))
+                    print(
+                        f"Ваша цифра:{numbers_for_thousands[number_list[0]]} тисяча{hundreds[number_list[1]]}{ordinary_numbers[decimals]}!")
+                else:
+                    print(
+                        f"Ваша цифра:{numbers_for_thousands[number_list[0]]} тисяча{hundreds[number_list[1]]}{decimal_numbers[number_list[2]]}{ordinary_numbers[number_list[3]]}!")
+            elif int(number_list[0]) in range(1, 5):
+                if int(number_list[-2]) == 1:
+                    decimal_list = number_list[-2:]
+                    decimals = str("".join(decimal_list))
+                    print(
+                        f"Ваша цифра:{numbers_for_thousands[number_list[0]]} тисячі{hundreds[number_list[1]]}{ordinary_numbers[decimals]}!")
+                else:
+                    print(
+                        f"Ваша цифра:{numbers_for_thousands[number_list[0]]} тисячі{hundreds[number_list[1]]}{decimal_numbers[number_list[2]]}{ordinary_numbers[number_list[3]]}!")
+            else:
+                if int(number_list[-2]) == 1:
+                    decimal_list = number_list[-2:]
+                    decimals = str("".join(decimal_list))
+                    print(
+                        f"Ваша цифра:{ordinary_numbers[number_list[0]]} тисяч{hundreds[number_list[1]]}{ordinary_numbers[decimals]}!")
+                else:
+                    print(
+                        f"Ваша цифра:{ordinary_numbers[number_list[0]]} тисяч{hundreds[number_list[1]]}{decimal_numbers[number_list[2]]}{ordinary_numbers[number_list[3]]}!")
     else:
-        print(
-            f"Ваша цифра:{hundreds[number_list[0]]}{decimal_numbers[number_list[1]]}{ordinary_numbers[number_list[2]]}!")
-elif int_prompt_number < 10000:
-    if int(number_list[-4]) in range(1, 5):
-        if int(number_list[-2]) == 1:
-            decimal_list = number_list[-2:]
-            decimals = str("".join(decimal_list))
-            print(
-                f"Ваша цифра:{numbers_for_thousands[number_list[0]]} тисячі{hundreds[number_list[1]]}{ordinary_numbers[decimals]}!")
-        else:
-            print(
-                f"Ваша цифра:{numbers_for_thousands[number_list[0]]} тисячі{hundreds[number_list[1]]}{decimal_numbers[number_list[2]]}{ordinary_numbers[number_list[3]]}!")
-    else:
-        if int(number_list[-2]) == 1:
-            decimal_list = number_list[-2:]
-            decimals = str("".join(decimal_list))
-            print(
-                f"Ваша цифра:{ordinary_numbers[number_list[0]]} тисячі{hundreds[number_list[1]]}{ordinary_numbers[decimals]}!")
-        else:
-            print(
-                f"Ваша цифра:{ordinary_numbers[number_list[0]]} тисяч{hundreds[number_list[1]]}{decimal_numbers[number_list[2]]}{ordinary_numbers[number_list[3]]}!")
+        return user_prompt("Ваше число менше 1 або більше 9999. Введіть число від 1 до 9999:")
+
+
+print(user_prompt("Введіть число від 1 до 9999: "))
