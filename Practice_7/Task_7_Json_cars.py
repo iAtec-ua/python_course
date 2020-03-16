91c9daf6fef419dba2d83581075717bba615be84
iAtec <atec.khm@gmail.com>
2020-03-16 15:54:34 +0200

    cars

--- Practice_7/Task_7_Json_cars.py
+++ Practice_7/Task_7_Json_cars.py
@@ -1,5 +1,19 @@
 # The script processes a file containing a list of cars and their features
 
+import json
+
 # Open the file for reading and print it
 file = open('Car_Model_List.json', 'r', encoding='utf-8')
-print(file.read())
+cars = file.read()
+print(cars)
+
+# Create a list of brands
+cars_dict = json.loads(cars)
+brands = []
+for i in cars_dict:
+    while i["Make"] not in brands:
+        brands.append(i["Make"])
+
+brands.sort()
+brands = ", ".join(brands)
+print(f"You can chose a brand of a car: {brands}")
