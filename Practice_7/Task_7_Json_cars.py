be9fff29edcbc256fb9789565eb2b1d829af0654
iAtec <atec.khm@gmail.com>
2020-03-16 15:04:54 +0200

    cars

---
+++ Practice_7/Task_7_Json_cars.py
@@ -0,0 +1,5 @@
+# The script processes a file containing a list of cars and their features
+
+# Open the file for reading and print it
+file = open('Car_Model_List.json', 'r', encoding='utf-8')
+print(file.read())
